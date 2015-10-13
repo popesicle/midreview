c153688bfec02df23e723cce15e2de47241112a8
Tyler Neustaedter <rtneust@gmail.com>
2015-10-13 09:39:11 -0500

    adds in the midreview homeworks.

---
+++ midreview.part2.Tyler.js
@@ -0,0 +1,125 @@
+var mocha = require('mocha'),
+    chai = require('chai')
+
+var assert = chai.assert;
+var expect = chai.expect;
+
+
+// 5. in a certain restaurant, a steak is $25, a bottle of wine is $40, a salad is $12, and a brownie sundae is $10. make a menu object that provides menu items as keys and prices as values. 
+
+// 6. use a constructor called Restaurant to create a Restaurant object. put a getPrice() method on the constructor's prototype that takes the name of a menu item as input and returns the price of the item. i should be able to test it like this:
+
+// >>> var applebees = new Menu()
+// >>> applebees.getPrice('salad')
+// >>> '$12'
+
+// 7. add a method to the Restaurant called getDishes. this method should read the Restaurant's menu object and turn it into an array of dish names, then return that array. (i.e., do not just hard-code an array containing the dish names).  
+
+// 8. write a function called getValues. getValues should take two inputs: an array of objects, and a property name. it should return an array containing the value of the specified property for each object. 
+
+// example: 
+// var colorDicts = [{red: 'rouge',blue:'bleu'},{red:'rojo',blue:'azul'},{red:'rote',blue:'blau'}]
+// getValues(colorDicts,'blue') --> ['bleu','azul','blau']
+
+
+// test for #5
+
+var menu = {
+    steak: '$25',
+    wine: '$40',
+    salad: '$12',
+    brownieSundae: '$10'
+}
+
+describe("menu",function(){
+    it('should be an object which yields the price of a menu item when the name of that item is supplied as a key.', function() {
+        expect(menu.salad).to.equal('$12')
+    })
+})
+
+
+// var applebees = new Restaurant()
+// 	applebees.menu = 
+	
+// test for #6 & #7
+
+var Restaurant = function(){
+   menu = {
+    steak: '$25',
+    wine: '$40',
+    salad: '$12',
+    brownieSundae: '$10'
+}
+    this.getPrice = function(string){
+        return(menu[string])
+    }
+}
+
+describe('Restaurant' , function() {
+    describe('#getPrice()',function(){
+        it('should return the price of an item from the menu' , function() {
+
+            expect(applebees.getPrice('salad')).to.equal('$12')
+            }
+        )
+    })
+})
+
+// test for #7
+
+var Restaurant = function(){
+
+   menu = {
+    steak: '$25',
+    wine: '$40',
+    salad: '$12',
+    brownieSundae: '$10'
+    }
+    
+    this.getPrice = function(string){
+        return(menu[string])
+    }
+
+    this.getDishes = function(){
+       
+}
+
+
+describe("pipeline()",function(){
+    it("should pipe the outputs of the functions passed in as arguments into a final value",function(){
+    	var squareNum = function(num) {return num * num},
+    		doubleNum = function(num) {return num * 2},
+    		inverseNum = function(num) {return 1 / num}
+
+        expect(pipeline(5,doubleNum,squareNum)).to.equal(100)
+        expect(pipeline(10,squareNum,inverseNum)).to.equal(.01)
+    })
+})
+
+// test for #8
+var getValues = function(initialArr, input){
+    var newArr = []
+    initialArr.forEach(function(object){
+        newArr.push(object[input])
+    })
+    return newArr
+}
+
+describe("getValues()",function(){
+    it("should pipe the outputs of the functions passed in as arguments into a final value",function(){
+
+            var books = [
+                            {author: "vladimir nabokov",
+                            title: "lolita",
+                            year: "1955"},
+                            {author: "jk rowling",
+                            title: "harry potter and the powerful skin rash",
+                            year: 2016},
+                            {author: "david foster wallace",
+                            title: "oblivion",
+                            year: 2002}
+                        ]
+
+            expect(getValues(books,'title')).to.deep.equal(["lolita","harry potter and the powerful skin rash", "oblivion"])
+    })
+})
---
+++ midreview.part3.Tyler.js
@@ -0,0 +1,73 @@
+var mocha = require('mocha'),
+    chai = require('chai')
+
+var assert = chai.assert;
+var expect = chai.expect;
+
+// 9. write a function called pipeline. it should take three inputs: (1) a string of text, (2) a function, and (3) another function. it should use functions (2) and (3) on the input, one after the other, and return a new string that has been processed by both function (2) and function (3).
+
+// for example, presuming i have a function called capitalize() and a function called removeFirstLetter, i should be able to do this
+
+// >>> var newWord = pipeline('america')
+// >>> 'Americ'
+
+// 10. write a function called map that will take an array and a callback as input.
+
+// 11. this one will be tricky and also really interesting. your task is to hack the built-in array method (hint: where do the methods on an array live?). replace map with a method that does ALMOST the same thing as the built-in map. the difference is, your map will add an extra element to the end: an "X." (feel free to replace the X with your own tag, but the mocha test will fail.)
+
+// 12. create a jquery promise/deferred object that will resolve true, and execute an associated callback, after 1 second. 
+
+// 13. create a constructor called Place. the important methods on place are .fetchData() and .getCoords(). fetchData should take the name of a place as input, make a request to an api of your choosing (hint: googlemaps geocoding), and store the latitude and longitude of the place in an object, retrievable with the getCoords method. 
+
+
+
+// test for #9
+
+var pipeline = function(initialVal,...callbacks){
+    var value = initialVal
+    callbacks.forEach(function(callback){
+        value = callback(value)
+        })
+    return value
+}
+
+describe("pipeline()",function(){
+    it("should pipe the outputs of the functions passed in as arguments into a final value",function(){
+    	var squareNum = function(num) {return num * num},
+    		doubleNum = function(num) {return num * 2},
+    		inverseNum = function(num) {return 1 / num}
+
+        expect(pipeline(5,doubleNum,squareNum)).to.equal(100)
+        expect(pipeline(10,squareNum,inverseNum)).to.equal(.01)
+    })
+})
+
+
+// test for #10
+
+var map = function(input, callback){
+    var arr = []
+    input.forEach(function(items){
+        arr.push(callback(items))
+    })
+    return arr
+}
+
+describe("map()",function(){
+    it("should return a new copy of the array after the given transformation",function(){
+
+    	expect(map([4,9,16],Math.sqrt)).to.deep.equal([2,3,4])
+    })
+})
+
+
+//test for #11
+describe("Array", function(){ // a Constructor name
+    describe("#map()", function(){ // a method of said Constructor
+		it("should behave as normal, except with an extra 'X' at the end of the array",function(){
+			expect([5.5,2.1,100.9].map(Math.floor)).to.deep.equal([5,2,100,"X"])
+		})
+	})
+})
+
+// no tests for the last two, as jquery is required, but i'll review them by sight. 
